659c20ae0e47a62782a871387142b5da09a8719b
Moaz Zardab <moazzardab@gmail.com>
2024-06-26 17:42:23 +0200

    database prep

--- run/src/main/java/com/run/run/run/RunRepository.java
+++ run/src/main/java/com/run/run/run/RunRepository.java
@@ -16,26 +16,33 @@ import jakarta.annotation.PostConstruct;
 public class RunRepository {
 
     private List<Run> runs = new ArrayList<>();
+    // NOTE: Commented parts dont use database coms
 
     // returns a specific run object identified by id (optional container object, returns isPresent bool)
+    /*
     Optional<Run> findById(int id) {
         return runs.stream()
             .filter(run -> run.id() == id) // CONDITION
             .findFirst();
     }
+    */
 
     // returns all run objects
+    /*
     List<Run> findAll() {
         return runs;
     }
+    */
 
     // --- Post (Create) ---
-
+    /*
     void create(Run run) {
         runs.add(run);  
     }
+    */
 
     // --- Put (update) ---
+    /*
     void update(Run run, int id) {
         Optional<Run> existingRun = findById(id);
 
@@ -44,16 +51,18 @@ public class RunRepository {
         }
 
     }
+    */
 
     // --- Delete ---
+    /*
     void delete(int id) {
         runs.removeIf(run -> run.id().equals(id));
     }
-
+    */
 
     @PostConstruct // method only happens after dependency injection
     private void init () {
-        runs.add(new Run(1, "First RUNNING", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR));
+        runs.add(new Run(2, "First RUNNING", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR));
     }
 
 }
